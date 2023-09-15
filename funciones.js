eef1dd299d3d6971e64acb5a986aa62a73536b1a
Franklin <franklinegonzales@gmail.com>
2023-09-14 20:58:13 -0500

    Agregamos archivo de Scope, se agrega informacion en el archivo funciones

--- funciones.js
+++ funciones.js
@@ -5,6 +5,7 @@
 function miFuncion() {
     return 3;
 }
+miFuncion();
 
 // Expresion
 
@@ -13,3 +14,15 @@ var miFuncion = function(a,b){
 }
 
 miFuncion();
+
+
+// Se declara la funcion con los parametros que espera recibir
+function sumar(a,b) {       
+    var resultado = a + b;  // se declara una variable que incluye la funcion de suma
+
+    return resultado;      // llamamos por medio de return para que muestre en pantalla el resultado de la variable.
+}
+
+
+// Para llamar la funcion se llama por nombre y declarar los valores que iran en los parametros
+sumar(1,2)
